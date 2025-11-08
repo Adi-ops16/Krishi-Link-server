aeb6344db5ee399a1d742349d388d014120c2b7c
Adi-ops16 <hasibadi22@gmail.com>
2025-11-09 03:26:17 +0600

    mongoDB connected to the server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -7,10 +9,34 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("assignment 10 server is running")
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@personal-hero.gxzvpbe.mongodb.net/?appName=Personal-Hero`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const DB = client.db("Krishi-Link-DB")
+        const cropsCollection = DB.collection('crops')
+
+        app.get('/', (req, res) => {
+            res.send("all working")
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`assignment 10 server is running on port: ${port}`)
+    console.log(`Krishi Link server is running on port: ${port}`)
 })
