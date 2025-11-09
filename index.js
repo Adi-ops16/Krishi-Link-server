82b7810ea59c91714d3bfa1e37cc0f8844e3f8c7
Adi-ops16 <hasibadi22@gmail.com>
2025-11-10 03:06:55 +0600

    added the all crops and crops by id APIs also api to post a crop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;
@@ -21,16 +21,47 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const DB = client.db("Krishi-Link-DB")
         const cropsCollection = DB.collection('crops')
 
-        app.get('/', (req, res) => {
-            res.send("all working")
+        // all crops API
+        app.get('/crops', async (req, res) => {
+            const limit = parseInt(req.query.limit);
+            const sortOrder = req.query.sort === 'asc' ? 1 : -1;
+
+            const result = await cropsCollection.find()
+                .sort({ created_at: sortOrder })
+                .limit(limit)
+                .toArray()
+
+            res.send(result)
         })
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // get crops by id
+        app.get('/crops/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await cropsCollection.findOne(query)
+            res.send(result)
+        })
+
+        // Add Crops API
+        app.post('/crops', async (req, res) => {
+            const cropData = req.body;
+
+            const newCrop = {
+                ...cropData,
+                interest: [],
+                created_at: new Date(),
+                updated_at: new Date()
+            }
+            const result = await cropsCollection.insertOne(newCrop)
+            res.send(result)
+        })
+
+        //  await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
     }
